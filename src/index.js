9d27237067b623da53665215128423448eb71973
oscarporras02 <oscarrussi@outlook.com>
2020-11-20 20:31:17 -0500

    add page load content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import pageLoad from './page-load.js';
 
 let pLoad = pageLoad();
+pLoad.loadNav();
+pLoad.loadDivHome();
+pLoad.loadDivMenu();
+pLoad.loadDivContacts();
 pLoad.loadHead();
 pLoad.loadImage();
 pLoad.loadParagraph();
--- src/page-load.js
+++ src/page-load.js
@@ -1,16 +1,44 @@
 export default function pageLoad(){
     return{
         divContent : document.getElementById('content'),
-        h3 : document.createElement("H3"),
+        nav : document.createElement("nav"),
+        divNav : document.createElement("div"),
+        divHome : document.createElement("div"),
+        divMenu : document.createElement("div"),
+        divContacts : document.createElement("div"),
+        h3 : document.createElement("h3"),
         image : document.createElement("img"),
         p : document.createElement("p"),
+        loadNav(){
+            debugger;
+            this.nav.className="navbar navbar-expand-lg navbar-dark bg-dark";
+            this.divNav.className="navbar-nav";
+            ["Home", "Menu", "Content"].forEach((item, index)=>{
+                let a = document.createElement("a");
+                a.text = item;
+                if(index==0) a.className = "nav-item nav-link active";
+                else a.className = "nav-item nav-link";
+                this.divNav.appendChild(a);
+            });
+            this.nav.appendChild(this.divNav);
+            this.divContent.appendChild(this.nav);
+        },
+        loadDivHome(){
+            this.divContent.appendChild(this.divHome);
+        },
+        loadDivMenu(){
+            this.divContent.appendChild(this.divMenu);
+        },
+        loadDivContacts(){
+            this.divContent.appendChild(this.divContacts);
+        },
         loadHead(){
             this.h3.innerText="Sushitaki";
-            this.divContent.appendChild(this.h3);
+            this.divHome.appendChild(this.h3);
         },
         loadImage(){
             this.image.src="../data/sushitaki.jpg";
-            this.divContent.appendChild(this.image);
+            this.divHome.appendChild(this.image);
         },
         loadParagraph(){
             this.p.innerText=`Japan has put raw fish on the culinary map. You can now find sushi across the world, from South America 
@@ -18,7 +46,7 @@ export default function pageLoad(){
             than its homeland. Originally scoffed at by the Michelin Guide, there are now sushi chefs with three stars, acclaimed for their 
             ingredients, technique and, of course, taste. From your classic sashimi to a full-on omakase menu, we pick the best of the best 
             sushi Japan has to offer.`;
-            this.divContent.appendChild(this.p);
+            this.divHome.appendChild(this.p);
         },
         loadContentStyles(){
             
