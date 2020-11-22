eb56c2c86069dea97071e8ed68bc621d65622471
oscarporras02 <oscarrussi@outlook.com>
2020-11-22 18:11:14 -0500

    align elements in the different tabs

--- src/contact-load.js
+++ src/contact-load.js
@@ -8,11 +8,23 @@ export default function contactLoad(){
         loadDivContact(){
             this.divContact.id="div2";
             this.divContact.classList.add("d-none");
+            this.divContact.classList.add("div-contact-styles");
+            this.formContact.classList.add("margin-form-items");
             this.divContent.appendChild(this.divContact);
             this.divContact.appendChild(this.headContact);
+            this.loadHeadContactContent();
+            this.loadparagraphContactContent();
             this.divContact.appendChild(this.paragraphContact);
             this.divContact.appendChild(this.formContact);
             this.loadForm();
+        },
+        loadHeadContactContent(){
+            this.headContact.classList.add("text-center");
+            this.headContact.innerText="Contact us";
+        },
+        loadparagraphContactContent(){
+            this.paragraphContact.classList.add("text-justify");
+            this.paragraphContact.innerText=`Provided you want to reach us out, please complete the next form. Any suggestion, complain, comment or business opportunitty you have on mind.`;
         },
         loadForm(){
             this.loadFormName();
